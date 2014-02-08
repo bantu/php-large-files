76477aff8b893e27c694ae872cdf75e866ede5a9
Andreas Fischer <bantu@owncloud.com>
2014-02-08 17:07:41 +0100

    Setup PHPUnit

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+* (c) Andreas Fischer <bantu@owncloud.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+
+require __DIR__.'/../vendor/autoload.php';
